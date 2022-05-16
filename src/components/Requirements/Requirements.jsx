9530f081d880e143a957ad3d5be4e778d0f3f705
Daniel <danielmaestreyepes@gmail.com>
2022-05-16 03:15:36 +0200

    implemented wallet removal from local storage, improved styling,fixed dropdowns, changed look to a more general way

--- src/components/Requirements/Requirements.jsx
+++ src/components/Requirements/Requirements.jsx
@@ -1,216 +1,269 @@
 import { useState, useEffect, Fragment } from "react";
-import { Menu, Transition } from '@headlessui/react'
-import wallet_pink from "../../assets/wallet_pink.png"
-import nautiusIcon from "../../assets/nautilus.jpg"
+import { Menu, Transition } from "@headlessui/react";
+import wallet_pink from "../../assets/wallet-dark.png";
+import nautiusIcon from "../../assets/nautilus.jpg";
 import WalletHover from "../WalletHover/WalletHover";
-import "./Requirements.css"
+import "./Requirements.css";
 
 function classNames(...classes) {
-	return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 const NANOERG_TO_ERG = 1000000000;
 const TOKENID_NO_TEST =
-	"afd0d6cb61e86d15f2a0adc1e7e23df532ba3ff35f8ba88bed16729cae933032";
+  "afd0d6cb61e86d15f2a0adc1e7e23df532ba3ff35f8ba88bed16729cae933032";
 const TOKENID_FAKE_SIGUSD =
-	"96c402c0e658909aa03f534006124f0e43725c467dbc8dea39680d0861892de5";
-    
-export const Requirements = ()=>{
-    const [open, setOpen] = useState(true);
-    const [ergoWallet, setErgoWallet] = useState();
-    const [ergBalance, setErgBalance] = useState(0);
-	const [sigUSDBalance, setSigUSDBalance] = useState(0);
-	const [owlBalance, setOwlBalance] = useState(0);
-	const [walletConnected, setWalletConnected] = useState(false);
-	const [showSelector, setShowSelector] = useState(false);
-	const [walletHover, setWalletHover] = useState(false);
-	const [readOnlyNautilus, setReadOnlyNautilus] = useState(false);
-    const [defaultAddress, setDefaultAddress] = useState();
+  "96c402c0e658909aa03f534006124f0e43725c467dbc8dea39680d0861892de5";
 
-    useEffect(() => {
-		const checkWallet = localStorage.getItem("walletConnected");
-		if (checkWallet === "true") {
-			window.ergoConnector.nautilus.connect().then((access_granted) => {
-				console.log("AFTER CONNECTION");
-				if (access_granted) {
-					setWalletConnected(true);
-					window.ergoConnector.nautilus.getContext().then((context) => {
-						console.log("nautilus is connected", context);
-						setErgoWallet(context);
-					});
-				} else {
-					setWalletConnected(false);
-					console.log("Wallet access denied");
-				}
-			});
-			window.addEventListener("ergo_wallet_disconnected", () => {
-				localStorage.setItem("walletAddress", "");
-				localStorage.setItem("walletConnected", "false");
-				setDefaultAddress(false);
-				setWalletConnected(false);
-				console.log("Wallet Disconnected!!!");
-			});
-			setDefaultAddress(
-				localStorage.getItem("walletAddress")
-			);
-			setWalletConnected(true);
-		}
-	}, []);
+export const Requirements = () => {
+  const [open, setOpen] = useState(true);
+  const [ergoWallet, setErgoWallet] = useState();
+  const [ergBalance, setErgBalance] = useState(0);
+  const [sigUSDBalance, setSigUSDBalance] = useState(0);
+  const [owlBalance, setOwlBalance] = useState(0);
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [showSelector, setShowSelector] = useState(false);
+  const [walletHover, setWalletHover] = useState(false);
+  const [readOnlyNautilus, setReadOnlyNautilus] = useState(false);
+  const [defaultAddress, setDefaultAddress] = useState();
 
-    useEffect(() => {
-		if (typeof ergoWallet !== "undefined") {
-			window.addEventListener("ergo_wallet_disconnected", () => {
-				localStorage.setItem("walletAddress", "");
-				localStorage.setItem("walletConnected", "");
-				setDefaultAddress(false);
-				setWalletConnected(false);
-				console.log("Wallet Disconnected!!!");
-			});
-			// get ERG balance
-			ergoWallet.get_balance().then(function (balance) {
-				setErgBalance(balance/NANOERG_TO_ERG);
-				console.log(`ERG: ${balance/NANOERG_TO_ERG}`);
-			});
-			// get SigUSD balance
-			ergoWallet.get_balance(TOKENID_FAKE_SIGUSD).then(function (balance) {
-				setSigUSDBalance(balance);
-				console.log(`SigUSD: ${balance/100}`);
-			});
-			// get OWL balance
-			ergoWallet.get_balance(TOKENID_NO_TEST).then(function (balance) {
-				setOwlBalance(balance);
-				console.log(`OWL: ${balance}`);
-			});
-			ergoWallet.get_change_address().then(function (address) {
-				console.log('ADDRESS: ', address)
-				localStorage.setItem("walletAddress", address);
-				setDefaultAddress(address);
-				localStorage.setItem("walletConnected", "true");
-			});
-		}
-	}, [ergoWallet]);
+  useEffect(() => {
+    const checkWallet = localStorage.getItem("walletConnected");
+    if (checkWallet === "true") {
+      window.ergoConnector.nautilus.connect().then((access_granted) => {
+        console.log("AFTER CONNECTION");
+        if (access_granted) {
+          setWalletConnected(true);
+          window.ergoConnector.nautilus.getContext().then((context) => {
+            console.log("nautilus is connected", context);
+            setErgoWallet(context);
+          });
+        } else {
+          setWalletConnected(false);
+          console.log("Wallet access denied");
+        }
+      });
+      window.addEventListener("ergo_wallet_disconnected", () => {
+        localStorage.setItem("walletAddress", "");
+        localStorage.setItem("walletConnected", "false");
+        setDefaultAddress(false);
+        setWalletConnected(false);
+        console.log("Wallet Disconnected!!!");
+      });
+      setDefaultAddress(localStorage.getItem("walletAddress"));
+      setWalletConnected(true);
+    }
+  }, []);
 
-    const disconnectWallet = () => {
-		if (typeof window.ergo_request_read_access === "undefined") {
-			console.log("Ergo not found");
-		} else {
-			if (walletConnected) {
-				setWalletConnected(false);
-				setErgoWallet();
-				setDefaultAddress("");
-				setOwlBalance(0);
-			}
-		}
-	};
+  useEffect(() => {
+    if (typeof ergoWallet !== "undefined") {
+      window.addEventListener("ergo_wallet_disconnected", () => {
+        localStorage.setItem("walletAddress", "");
+        localStorage.setItem("walletConnected", "");
+        setDefaultAddress(false);
+        setWalletConnected(false);
+        console.log("Wallet Disconnected!!!");
+      });
+      // get ERG balance
+      ergoWallet.get_balance().then(function (balance) {
+        setErgBalance(balance / NANOERG_TO_ERG);
+        console.log(`ERG: ${balance / NANOERG_TO_ERG}`);
+      });
+      // get SigUSD balance
+      ergoWallet.get_balance(TOKENID_FAKE_SIGUSD).then(function (balance) {
+        setSigUSDBalance(balance);
+        console.log(`SigUSD: ${balance / 100}`);
+      });
+      // get OWL balance
+      ergoWallet.get_balance(TOKENID_NO_TEST).then(function (balance) {
+        setOwlBalance(balance);
+        console.log(`OWL: ${balance}`);
+      });
+      ergoWallet.get_change_address().then(function (address) {
+        console.log("ADDRESS: ", address);
+        localStorage.setItem("walletAddress", address);
+        setDefaultAddress(truncate(address, 17, "..."));
+        localStorage.setItem("walletConnected", "true");
+      });
+    }
+  }, [ergoWallet]);
 
-	const toggleSelector = () => {
-		if (!walletConnected) setShowSelector(!showSelector);
-	};
+  const truncate = (str, len, sep) => {
+    if (str.length < len) {
+      return str;
+    } else {
+      return (
+        str.substring(0, parseInt(len / 2)) +
+        sep +
+        str.substring(str.length - parseInt(len / 2), str.length)
+      );
+    }
+  };
 
-	const handleWalletTrue = () => {
-		if (walletConnected) setWalletHover((prev) => !prev);
-		else {
-			setShowSelector((prev) => !prev);
-		}
-	};
+  const disconnectWallet = () => {
+    if (typeof window.ergo_request_read_access === "undefined") {
+      console.log("Ergo not found");
+    } else {
+      if (walletConnected) {
+        setWalletConnected(false);
+        setErgoWallet();
+        setDefaultAddress("");
+        setOwlBalance(0);
+		localStorage.removeItem("walletAddress");
+		localStorage.removeItem("walletConnected");
+      }
+    }
+  };
 
-    const connectNautilus = () => {
-		if(!window.ergoConnector){
-			return;
-		}
-		window.ergoConnector.nautilus.isConnected().then((connected) => {
-			if (!walletConnected) {
-				console.log("WALLET CREDENTIALS: ", connected);
-				console.log("BEFORE CONNECTION");
-				window.ergoConnector.nautilus.connect().then((access_granted) => {
-					console.log("AFTER CONNECTION");
-					if (access_granted) {
-						setWalletConnected(true);
-						window.ergoConnector.nautilus.getContext().then((context) => {
-							console.log("nautilus is connected", context);
-							setErgoWallet(context);
-						});
-					} else {
-						setWalletConnected(false);
-						console.log("Wallet access denied");
-					}
-				});
-				toggleSelector();
-			} else {
-				// Already connected
-				console.log(`nautilus is connected`);
-				toggleSelector();
-				return;
-			}
-		});
-	};
+  const toggleSelector = () => {
+    if (!walletConnected) setShowSelector(!showSelector);
+  };
 
-    return(
-        <>
-			{showSelector && (
-				<Menu as="div" className="mainDiv">
+  const handleWalletTrue = () => {
+    if (walletConnected) setWalletHover((prev) => !prev);
+    else {
+      setShowSelector((prev) => !prev);
+    }
+  };
 
-				<Transition
-				  show={open}
-				  as={Fragment}
-				  enter="transition ease-out duration-100"
-				  enterFrom="transform opacity-0 scale-95"
-				  enterTo="transform opacity-100 scale-100"
-				  leave="transition ease-in duration-75"
-				  leaveFrom="transform opacity-100 scale-100"
-				  leaveTo="transform opacity-0 scale-95"
-				>
-				  <Menu.Items className="mainMenuItem" style={{ marginTop:'2.5rem', position:'absolute'}}>
-					<div style={{padding:'0.25rem 0 0.25rem', marginBottom:'1px'}}>
-					  <Menu.Item onClick={connectNautilus}>
-						{({ active }) => (
-						  <a
-							href="#"
-							className={classNames(
-							  active ? 'item1' : 'item2',
-							  'item3'
-							)}
-						  >
-							<img src={nautiusIcon} style={{height:'30px', marginRight:'3rem'}} />
-							Nautilus
-						  </a>
-						)}
-					  </Menu.Item>   
-					</div>
-				   
-				  </Menu.Items>
-				</Transition>
-			  </Menu>
-			)}
-			
-			<div id="header-wallet-wrapper" onClick={handleWalletTrue}>
-				<div id="header-wallet" style={{flexDirection:walletConnected?'column':'row'}}>
-					{!walletConnected && <img src={wallet_pink} id="header-wallet-image" />}
-					<div id="wallet-connect">
-						<span>
-							{walletConnected ? <span style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-								{/* <p style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center',fontWeight:'bold'}}>
+  const connectNautilus = () => {
+    if (!window.ergoConnector) {
+      return;
+    }
+    window.ergoConnector.nautilus.isConnected().then((connected) => {
+      if (!walletConnected) {
+        console.log("WALLET CREDENTIALS: ", connected);
+        console.log("BEFORE CONNECTION");
+        window.ergoConnector.nautilus.connect().then((access_granted) => {
+          console.log("AFTER CONNECTION");
+          if (access_granted) {
+            setWalletConnected(true);
+            window.ergoConnector.nautilus.getContext().then((context) => {
+              console.log("nautilus is connected", context);
+              setErgoWallet(context);
+            });
+          } else {
+            setWalletConnected(false);
+            console.log("Wallet access denied");
+          }
+        });
+        toggleSelector();
+      } else {
+        // Already connected
+        console.log(`nautilus is connected`);
+        toggleSelector();
+        return;
+      }
+    });
+  };
+
+  return (
+    <div className="package-container">
+      {showSelector && (
+        <Menu as="div" className="mainDiv">
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              className="mainMenuItem"
+              style={{ marginTop: "2.5rem", position: "absolute" }}
+            >
+              <div
+                style={{ padding: "0.25rem 0 0.25rem", marginBottom: "1px" }}
+              >
+                <Menu.Item onClick={connectNautilus}>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? "item1" : "item2",
+                        "item3"
+                      )}
+                    >
+                      <img
+                        src={nautiusIcon}
+                        style={{
+                          height: "30px",
+                          marginRight: "3rem",
+                          marginLeft: "0.5rem",
+                        }}
+                      />
+                      Nautilus
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu>
+      )}
+
+      <div id="header-wallet-wrapper" onClick={handleWalletTrue}>
+        <div
+          id="header-wallet"
+          style={{ flexDirection: walletConnected ? "column" : "row" }}
+        >
+          {!walletConnected && (
+            <img src={wallet_pink} id="header-wallet-image" />
+          )}
+          <div id="wallet-connect">
+            <span>
+              {walletConnected ? (
+                <span
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  {/* <p style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center',fontWeight:'bold'}}>
 									{owlBalance}
 									<span>OWL</span>
 								</p> */}
-								<span style={{display:'flex', justifyContent:'space-evenly', alignItems:'center', gap:'5px'}}>
-									<img src={nautiusIcon} style={{height:'20px'}} />
-									<p style={{color:'black', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap', width:'150px'}}>{defaultAddress}</p>
-								</span>
-
-								</span> : "Connect Wallet"}
-						</span>
-					</div>
-					{(walletHover && walletConnected) &&(
-						<WalletHover
-							disconnect={disconnectWallet}
-							owlBalance={owlBalance}
-							sigUSDBalance={sigUSDBalance}
-							ergBalance={ergBalance}
-						/>
-					)}
-				</div>
-			</div>
-		</>
-    )
-}
+                  <span
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-evenly",
+                      alignItems: "center",
+                      gap: "5px",
+                    }}
+                  >
+                    <img src={nautiusIcon} style={{ height: "20px" }} />
+                    <p
+                      style={{
+                        color: "black",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        whiteSpace: "nowrap",
+                        width: "150px",
+                      }}
+                    >
+                      {defaultAddress}
+                    </p>
+                  </span>
+                </span>
+              ) : (
+                "Connect Wallet"
+              )}
+            </span>
+          </div>
+          {walletHover && walletConnected && (
+            <WalletHover
+              disconnect={disconnectWallet}
+              owlBalance={owlBalance}
+              sigUSDBalance={sigUSDBalance}
+              ergBalance={ergBalance}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/WalletHover/WalletHover.jsx
+++ src/components/WalletHover/WalletHover.jsx
@@ -1,28 +1,25 @@
-import { Fragment, useState } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import {
-  ArchiveIcon,
-  ArrowCircleRightIcon,
-  ChevronDownIcon,
-  DuplicateIcon,
-  HeartIcon,
-  PencilAltIcon,
-  TrashIcon,
-  UserAddIcon,
-} from '@heroicons/react/solid'
+import { Fragment, useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import SigRSVicon from "../../assets/sigrsv-icon.svg";
+import ERGicon from "../../assets/ergo-icon.svg";
 
-import '../Requirements/Requirements.css'
+import "../Requirements/Requirements.css";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
-export default function WalletHover({ disconnect, owlBalance, sigUSDBalance, ergBalance }) {
-    const [open, setOpen] = useState(true);
+export default function WalletHover({
+  disconnect,
+  owlBalance,
+  sigUSDBalance,
+  ergBalance,
+
+}) {
+  const [open, setOpen] = useState(true);
 
   return (
     <Menu as="div" className="mainDiv">
-
       <Transition
         show={open}
         as={Fragment}
@@ -34,53 +31,76 @@ export default function WalletHover({ disconnect, owlBalance, sigUSDBalance, erg
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="mainMenuItem">
-          <div style={{padding:'0.25rem 0 0.25rem', marginBottom:'1px'}}>
+          <div style={{ padding: "0.25rem 0 0.25rem", marginBottom: "1px" }}>
+            {ergBalance != 0 && (
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(active ? "item1" : "item2", "item3")}
+                  >
+                    <img src={ERGicon} className="token-icon-img" />
+                    <p>
+                      ERG Balance:
+                      <br />
+                      {ergBalance} ERG
+                    </p>
+                  </a>
+                )}
+              </Menu.Item>
+            )}
+            {sigUSDBalance != 0 && (
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(active ? "item1" : "item2", "item3")}
+                  >
+                    <img src={SigRSVicon} className="token-icon-img" />
+                    <p>
+                      SigUSD Balance:
+                      <br />
+                      {sigUSDBalance} SigUSD
+                    </p>
+                  </a>
+                )}
+              </Menu.Item>
+            )}
+            {owlBalance != 0 && (
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(active ? "item1" : "item2", "item3")}
+                  >
+                    <img src={ERGicon} className="token-icon-img" />
+                    <p>
+                      OWL Balance:
+                      <br />
+                      {owlBalance} OWL
+                    </p>
+                  </a>
+                )}
+              </Menu.Item>
+            )}
+
             <Menu.Item>
               {({ active }) => (
                 <a
+                  style={{ textAlign: "center" }}
                   href="#"
-                  className={classNames(
-                    active ? 'item1' : 'item2',
-                    'item3'
-                  )}
+                  onClick={disconnect}
+                  className={classNames(active ? "item1" : "item2", "item3")}
                 >
-                  <ArchiveIcon className="icon" aria-hidden="true" />
-                  Wallet Ballance - {ergBalance} ERG
+                  <p style={{ color: "rgba(205, 10, 10, 0.8)", margin: "0 auto" , fontSize:"0.95rem"}}>
+                    Clear Wallet
+                  </p>
                 </a>
               )}
             </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'item1' : 'item2',
-                    'item3'
-                  )}
-                >
-                  <ArchiveIcon className="icon" aria-hidden="true" />
-                  Wallet Ballance - {sigUSDBalance} SigUSD
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
-                  className={classNames(
-                    active ? 'item1' : 'item2',
-                    'item3'
-                  )}
-                >
-                  <ArchiveIcon className="icon" aria-hidden="true" />
-                  Wallet Ballance - {owlBalance} OWL
-                </a>
-              )}
-            </Menu.Item>            
           </div>
-         
         </Menu.Items>
       </Transition>
     </Menu>
-  )
+  );
 }
